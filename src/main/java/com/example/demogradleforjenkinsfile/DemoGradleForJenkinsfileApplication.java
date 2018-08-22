92ba04825cfdaef0cbce66c3b2c84670d2b16472
ANISH NAGARAJ <anish.nagaraj@LP-C02WQ0BSG8WL.local>
2018-08-22 13:12:29 +0200

    First commit

---
+++ src/main/java/com/example/demogradleforjenkinsfile/DemoGradleForJenkinsfileApplication.java
@@ -0,0 +1,12 @@
+package com.example.demogradleforjenkinsfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleForJenkinsfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleForJenkinsfileApplication.class, args);
+	}
+}
